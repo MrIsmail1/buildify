2a0d57e82ae0536705914a77b0be886e04470861
Yassine <yassineboul98@gmail.com>
2023-06-09 03:32:12 +0200

    Signed commit

---
+++ site/Views/dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tableau de bord</title>
+    <!-- Ici, vous pouvez ajouter du CSS pour styliser votre tableau de bord. -->
+</head>
+<body>
+    <h1>Tableau de bord</h1>
+    <!-- Ici, vous pouvez ajouter du HTML pour afficher les données de votre tableau de bord. -->
+</body>
+</html>
