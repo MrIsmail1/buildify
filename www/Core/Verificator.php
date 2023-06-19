ef6729f6ed49e8ea4865aafc4dc1fce9cfbb7477
Ismail Mrabet <ismail.mrabet1@gmail.com>
2023-06-19 11:54:13 +0200

    fix: form validation
    Signed-off-by: Ismail Mrabet <ismail.mrabet1@gmail.com>

--- www/Core/Verificator.php
+++ www/Core/Verificator.php
@@ -29,7 +29,7 @@ class Verificator
                 $errors[] = $configInput["error"];
             }
             if ($configInput["type"] == "email" && !self::checkEmail($data[$name])) {
-                $listOfErrors[] = $configInput["error"];
+                $errors[] = $configInput["error"];
             }
             if ($configInput["type"] == "password" &&  !self::checkPwd($data[$name]) && empty($input["passwordConfirm"])) {
                 $errors[] = $configInput["error"];
@@ -61,46 +61,3 @@ class Verificator
             && preg_match("/[A-Z]/", $pwd, $result);
     }
 }
-/* namespace App\Core;
-
-class Verificator
-{
-    public static function form(array $config, array $data): array
-    {
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $password = $_POST['password'];
-        $passwordConfirm = $_POST['passwordConfirm'];
-        $listOfErrors = [];
-
-        if (count($config["inputs"]) != count($data) - 1) {
-            throw new \Exception("Le nombre de champs dans la configuration correspond au nombre de champs dans les données");
-        }
-
-        foreach ($config["inputs"] as $name => $input) {
-            if (empty($data[$name])) {
-                throw new \Exception("Le champ est vide");
-            }
-
-            if ($password !== $passwordConfirm) {
-                throw new \Exception("Password and Confirm Password are not the same");
-            }
-
-            if (strlen($password) < 8 || !preg_match("/\d/", $password)) {
-                throw new \Exception("Password needs to have at least 8 characters with 1 number");
-            }
-
-            if ($input["type"] == "email" && !self::checkEmail($data[$name])) {
-                $listOfErrors[] = $input["error"];
-            }
-
-            if (!preg_match("/^[a-zA-Z]+$/", $firstname) || !preg_match("/^[a-zA-Z]+$/", $lastname)) {
-                throw new \Exception("First name or last name contains forbidden characters");
-            }
-        }
-
-        return $listOfErrors;
-    }
-
-
-} */
